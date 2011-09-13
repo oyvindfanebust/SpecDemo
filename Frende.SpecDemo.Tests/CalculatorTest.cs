1b2e78625e37f77618a46a997884c11b775d411d
fanoyv <oyvind.fanebust@reaktor.no>
2011-09-13 10:50:16 +0200

    Added unit tests for calculator

---
+++ Frende.SpecDemo.Tests/CalculatorTest.cs
@@ -0,0 +1,49 @@
+﻿using Should;
+using Xunit;
+
+namespace Frende.SpecDemo.Tests
+{
+	public class CalculatorTest
+	{
+		[Fact]
+		public void cappucino_should_cost_20_kr()
+		{
+			var calculator = new Calculator();
+
+			int price = calculator.GetPrice("cappucino");
+
+			price.ShouldEqual(20);
+		}
+
+		[Fact]
+		public void double_espresso_should_cost_20_kr()
+		{
+			var calculator = new Calculator();
+
+			int price = calculator.GetPrice("dbl_espresso");
+
+			price.ShouldEqual(20);
+		}
+
+		[Fact]
+		public void single_espresso_should_cost_10_kr()
+		{
+			var calculator = new Calculator();
+
+			int price = calculator.GetPrice("espresso");
+
+			price.ShouldEqual(10);
+		}
+	}
+
+	public class Calculator
+	{
+		public int GetPrice(string product)
+		{
+			if (product.ToLower().Equals("espresso"))
+				return 10;
+
+			return 20;
+		}
+	}
+}
