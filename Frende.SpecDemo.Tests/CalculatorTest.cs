0308dc59324856d506dca36568665e1af531d19c
fanoyv <oyvind.fanebust@reaktor.no>
2011-09-13 10:56:34 +0200

    Refactored tests

--- Frende.SpecDemo.Tests/CalculatorTest.cs
+++ Frende.SpecDemo.Tests/CalculatorTest.cs
@@ -1,49 +1,22 @@
-﻿using Should;
-using Xunit;
+﻿using Frende.SpecDemo.Web.Domain;
+using Should;
+using Xunit.Extensions;
 
 namespace Frende.SpecDemo.Tests
 {
 	public class CalculatorTest
 	{
-		[Fact]
-		public void cappucino_should_cost_20_kr()
+		[Theory]
+		[InlineData("cappucino", 20)]
+		[InlineData("dbl_espresso", 20)]
+		[InlineData("espresso", 10)]
+		public void product_should_cost(string product, int expectedPrice)
 		{
 			var calculator = new Calculator();
 
-			int price = calculator.GetPrice("cappucino");
+			int price = calculator.GetPrice(product);
 
-			price.ShouldEqual(20);
-		}
-
-		[Fact]
-		public void double_espresso_should_cost_20_kr()
-		{
-			var calculator = new Calculator();
-
-			int price = calculator.GetPrice("dbl_espresso");
-
-			price.ShouldEqual(20);
-		}
-
-		[Fact]
-		public void single_espresso_should_cost_10_kr()
-		{
-			var calculator = new Calculator();
-
-			int price = calculator.GetPrice("espresso");
-
-			price.ShouldEqual(10);
-		}
-	}
-
-	public class Calculator
-	{
-		public int GetPrice(string product)
-		{
-			if (product.ToLower().Equals("espresso"))
-				return 10;
-
-			return 20;
+			price.ShouldEqual(expectedPrice);
 		}
 	}
 }
---
+++ Frende.SpecDemo.Web/Domain/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace Frende.SpecDemo.Web.Domain
+{
+	public class Calculator
+	{
+		public int GetPrice(string product)
+		{
+			if (product.ToLower().Equals("espresso"))
+				return 10;
+
+			return 20;
+		}
+	}
+}
